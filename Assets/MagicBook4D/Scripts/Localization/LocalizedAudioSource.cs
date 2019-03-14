3e936e0ea7e82101e6093513aeee3a0392115dd2
ga_beo <hoavk.firecoals@gmail.com>
2019-03-14 09:42:52 +0700

    no message

--- Assets/MagicBook4D/Scripts/Localization/LocalizedAudioSource.cs
+++ Assets/MagicBook4D/Scripts/Localization/LocalizedAudioSource.cs
@@ -1,10 +1,13 @@
-﻿using System.Collections;
+﻿using Lean.Localization;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class LocalizedAudioSource : MonoBehaviour
 {
+    public LeanLocalization Target;
 
+    public AudioSource audioSource;
 
     // Start is called before the first frame update
     void Start()
@@ -16,5 +19,25 @@ public class LocalizedAudioSource : MonoBehaviour
     void Update()
     {
         
+    }
+
+    public void CreateNewPhrase(string languageName, string phraseName)
+    {
+        var target = Target;
+
+        if (target == null && LeanLocalization.AllLocalizations.Count > 0)
+        {
+            target = LeanLocalization.AllLocalizations[0];
+        }
+
+        // Create a new LeanLocalization?
+        if (target == null)
+        {
+            target = new GameObject("LeanLocalization").AddComponent<LeanLocalization>();
+        }
+
+        bool updateLanguage = false;
+
+        var translation = target.AddTranslation(languageName, phraseName);
     }
 }
