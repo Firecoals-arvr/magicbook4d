b9e6808d3eb0a6a301d9e3c37c307e44158dfaca
firecoalsjsc <firecoalsjsc@gmail.com>
2019-03-15 22:11:57 +0700

    edit download assetbunlde with only 1 line code Load Asset From assetbundle in 2 way: PreLoad (pre load assetbundle thus you can load bundle synchronous) and Load assetbundle Asynchronous Example in Scene/Test/TestScene.unity

--- Assets/MagicBook4D/Scripts/AssetBundles/DownLoadAssetBundles.cs
+++ Assets/MagicBook4D/Scripts/AssetBundles/DownLoadAssetBundles.cs
@@ -8,24 +8,52 @@ using Random = UnityEngine.Random;
 
 namespace Firecoals.AssetBundles
 {
-    public class DownLoadAssetBundles
+    public class DownLoadAssetBundles : MyBundleResources
     {
-        private IResources resources;
         private IDownloader downloader;
         private bool downloading = false;
         private static string platformName;
+        private Dictionary<string, IBundle> bundles = new Dictionary<string, IBundle>();
         //private string bundleUrl;
         //private void Start()
         //{
         //    Uri baseUri = new Uri(bundleUrl);
         //    this.downloader = new WWWDownloader(baseUri, true);
         //}
-        public DownLoadAssetBundles(string bundleUrl)
+        #region DownLoadAssetBundle
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="bookName">link from amazon s3, for example: https://s3-ap-southeast-1.amazonaws.com/magicbook4d/Android/Animal/bundles/ Animal is book name </param>
+        /// <param name="bundleRoot"></param>
+        public DownLoadAssetBundles(string bookName, string bundleRoot)
         {
-            Uri baseUri = new Uri(bundleUrl);//"https://s3-ap-southeast-1.amazonaws.com/magicbook4d/Android/Animal/bundles/"
+            BundleSetting bundleSetting = new BundleSetting(bundleRoot);
+            Uri baseUri = new Uri(GetDataUrl(bookName));
             this.downloader = new WWWDownloader(baseUri, false);
         }
 
+        protected override IResources GetResources()
+        {
+            return base.GetResources();
+        }
+        /// <summary>
+        /// Return the S3 Amazon Storage Url 
+        /// </summary>
+        /// <param name="bookName">Animal, Space, Color</param>
+        /// <returns></returns>
+        private string GetDataUrl(string bookName)
+        {
+#if UNITY_ANDROID
+            platformName = "Android";
+#endif
+#if UNITY_IOS
+        platformName = "IOS";
+#endif
+            var currentVersion = Application.version;
+            var url = "https://s3-ap-southeast-1.amazonaws.com/magicbook4d/" + currentVersion + "/" + platformName + "/" + bookName + "/bundles/";
+            return url;
+        }
         public IEnumerator Download()
         {
             this.downloading = true;
@@ -88,131 +116,11 @@ namespace Firecoals.AssetBundles
                 this.downloading = false;
             }
         }
-
-        IResources GetResources()
-        {
-            if (this.resources != null)
-                return this.resources;
-
-            /* Create a BundleManifestLoader. */
-            IBundleManifestLoader manifestLoader = new BundleManifestLoader();
-
-            /* Loads BundleManifest. */
-            BundleManifest manifest = manifestLoader.Load(BundleUtil.GetStorableDirectory() + BundleSetting.ManifestFilename);
-
-            //manifest.ActiveVariants = new string[] { "", "sd" };
-            //manifest.ActiveVariants = new string[] { "", "hd" };
-
-            /* Create a PathInfoParser. */
-            IPathInfoParser pathInfoParser = new AutoMappingPathInfoParser(manifest);
-
-            /* Use a custom BundleLoaderBuilder */
-            ILoaderBuilder builder = new CustomBundleLoaderBuilder(new Uri(BundleUtil.GetReadOnlyDirectory()), false);
-
-            /* Create a BundleManager */
-            IBundleManager manager = new BundleManager(manifest, builder);
-
-            /* Create a BundleResources */
-            this.resources = new BundleResources(pathInfoParser, manager);
-            return this.resources;
-        }
-        /// <summary>
-        /// Load GameObject
-        /// </summary>
-        /// <param name="name"></param>
-        public void LoadAsset(string name)
-        {
-            var resources = this.GetResources();
-            IProgressResult<float, GameObject> result = resources.LoadAssetAsync<GameObject>(name);
-            result.Callbackable().OnCallback((r) =>
-            {
-                try
-                {
-                    if (r.Exception != null)
-                        throw r.Exception;
-
-                    GameObject.Instantiate(r.Result, new Vector3(Random.Range(-1,1), Random.Range(-1, 1)), Quaternion.identity);
+        #endregion
 
 
-                }
-                catch (Exception e)
-                {
-                    Debug.LogErrorFormat("Load failure.Error:{0}", e);
-                }
-            });
-        }
-        /// <summary>
-        /// Load An Object from assetbundle
-        /// </summary>
-        /// <param name="name"></param>
-        /// <returns></returns>
-        public UnityEngine.Object LoadAssetObject(string name)
-        {
-            var resources = this.GetResources();
-            IProgressResult<float, UnityEngine.Object> result = resources.LoadAssetAsync<UnityEngine.Object>(name);
-            UnityEngine.Object @object = null;
-            result.Callbackable().OnCallback((r) =>
-            {
-                try
-                {
-                    if (r.Exception != null)
-                        throw r.Exception;
-
-                    @object =  r.Result;
-
-                }
-                catch (Exception e)
-                {
-                    Debug.LogErrorFormat("Load failure.Error:{0}", e);
-                }
-            });
-            return @object;
-        }
-        /// <summary>
-        /// Load all Objects of a assetbundle name
-        /// </summary>
-        /// <param name="name"></param>
-        /// <returns></returns>
-        public UnityEngine.Object[] LoadAllAssetObject(string name)
-        {
-            var resources = this.GetResources();
-            IProgressResult<float, UnityEngine.Object[]> result = resources.LoadAllAssetsAsync<UnityEngine.Object>(name);
-            UnityEngine.Object[] @object = null;
-            result.Callbackable().OnCallback((r) =>
-            {
-                try
-                {
-                    if (r.Exception != null)
-                        throw r.Exception;
-
-                    @object = r.Result;
-
-                }
-                catch (Exception e)
-                {
-                    Debug.LogErrorFormat("Load failure.Error:{0}", e);
-                }
-            });
-            return @object;
-        }
-
-        /// <summary>
-        /// Return the S3 Amazon Storage Url 
-        /// </summary>
-        /// <param name="bookName">Animal, Space, Color</param>
-        /// <returns></returns>
-        public static string GetDataUrl(string bookName)
-        {
-#if UNITY_ANDROID
-            platformName = "Android";
-#endif
-#if UNITY_IOS
-        platformName = "IOS";
-#endif
-            var currentVersion = Application.version;
-            var url = "https://s3-ap-southeast-1.amazonaws.com/magicbook4d/" + currentVersion + "/" + platformName + "/" + bookName + "/bundles/";
-            return url;
-        }
     }
+
+
 }
 
