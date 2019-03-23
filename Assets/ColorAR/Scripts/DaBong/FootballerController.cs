2648fcc9b4b1407a565be9c8f8b971334be14002
ga_beo <hoavk.firecoals@gmail.com>
2019-03-23 19:11:22 +0700

    Map color and setup animation.

---
+++ Assets/ColorAR/Scripts/DaBong/FootballerController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Firecoals.Color
+{
+	public class FootballerController : MonoBehaviour
+	{
+		// Start is called before the first frame update
+		void Start()
+		{
+
+		}
+
+		// Update is called once per frame
+		void Update()
+		{
+
+		}
+	}
+}
+
---
+++ Assets/ColorAR/Scripts/MayBay/ActionsController.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Firecoals.Color
+{
+	/// <summary>
+	/// 
+	/// </summary>
+	public class ActionsController : MonoBehaviour
+	{
+		public GameObject groupMayBay, propeller;
+
+		// Start is called before the first frame update
+		void Start()
+		{
+			//CreateAnimation();
+		}
+
+		// Update is called once per frame
+		void Update()
+		{
+			//HideObjectInfo();
+			RotatePropeller();
+		}
+
+		public void CreateAnimation()
+		{
+			//groupMayBay.transform.RotateAround(new Vector3(0, 20f, 0), Vector3.left, 90 * Time.deltaTime);
+			StartCoroutine(RotateAirplane());
+		}
+
+		public void RotatePropeller()
+		{
+			propeller.transform.Rotate(new Vector3(0, 0, 200) * Time.deltaTime, Space.Self);
+		}
+
+		private void HideObjectInfo()
+		{
+			if (Input.GetMouseButtonDown(0))
+			{
+				Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+				if (Physics.Raycast(ray, out RaycastHit hit))
+				{
+					if (hit.transform.tag == "Airplane")
+					{
+						CreateAnimation();
+					}
+				}
+			}
+		}
+
+		public IEnumerator RotateAirplane()
+		{
+			float duration = 2f;
+
+			float counter = 0f;
+
+			while (counter < duration)
+			{
+				counter += Time.deltaTime;
+				groupMayBay.transform.RotateAround(new Vector3(0, 0.1f, 0), Vector3.right, 180 * Time.deltaTime);
+				yield return null;
+			}
+		}
+	}
+}
+
+
