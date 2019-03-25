c6e410c931b25a8bb1eb71c9bd56ef1912cff3e0
firecoals <datnguyen.firecoals@gmail.com>
2019-03-25 22:53:36 +0700

    Add InstantiateAsync On Firecoals.Augmentation Add Dispatcher.cs On Firecoals.Augmentation

---
+++ Assets/MagicBook4D/Scripts/Augment/Dispatcher.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dispatcher : MonoBehaviour
+{
+    private static Dispatcher instance;
+
+    private readonly List<Action> _pending = new List<Action>();
+
+    public static Dispatcher Instance
+    {
+        get
+        {
+            return instance;
+        }
+    }
+
+    public void Invoke(Action fn)
+    {
+        lock (this._pending)
+        {
+            this._pending.Add(fn);
+        }
+    }
+
+    private void InvokePending()
+    {
+        lock (this._pending)
+        {
+            foreach (Action action in this._pending)
+            {
+                action();
+            }
+
+            this._pending.Clear();
+        }
+    }
+
+    private void Awake()
+    {
+        if (instance != null && instance != this)
+        {
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            instance = this;
+        }
+    }
+
+    private void Update()
+    {
+        this.InvokePending();
+    }
+}
