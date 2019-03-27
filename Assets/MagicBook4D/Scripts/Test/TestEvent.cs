f8ccb007f9e1780d45e6411f5d59c5934e212dd6
firecoals <datnguyen.firecoals@gmail.com>
2019-03-27 17:25:44 +0700

    update UI

---
+++ Assets/MagicBook4D/Scripts/Test/TestEvent.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestEvent : MonoBehaviour
+{
+    public void ClickShowTooltip()
+    {
+        UITooltip.Show("Bầu trời trong xanh thăm thẳm, không một gợn mây.");
+    }
+}
+
