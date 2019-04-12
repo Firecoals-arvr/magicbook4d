8f3b59fdcaef77cb361670e1e6775ff35b81d712
cuong1602 <cuongngo.firecoals@gmail.com>
2019-04-12 13:44:25 +0700

    sửa Animation Color

--- Assets/ColorAR/Scripts/TrangTrai/FarmAnimations.cs
+++ Assets/ColorAR/Scripts/TrangTrai/FarmAnimations.cs
@@ -12,23 +12,13 @@ namespace Firecoals.Color
             all.GetComponent<Animator>().SetTrigger("Move");
             pig.GetComponent<Animator>().SetTrigger("Move");
             cow.GetComponent<Animator>().SetTrigger("Move");
-            StartCoroutine(PlayAnim2());
             StartCoroutine(StopAnim());
-
         }
         
-        IEnumerator PlayAnim2()
-        {
-            yield return new WaitForSeconds(6.2f);
-            all.GetComponent<Animator>().SetTrigger("Move2");
-            pig.GetComponent<Animator>().SetTrigger("Move");
-            cow.GetComponent<Animator>().SetTrigger("Move");
-        }
+        
         IEnumerator StopAnim()
         {
-            yield return new WaitForSeconds(5f);
-            Debug.Log("abc");
-            all.GetComponent<Animator>().SetTrigger("Idle");
+            yield return new WaitForSeconds(30f);
             pig.GetComponent<Animator>().SetTrigger("Idle");
             cow.GetComponent<Animator>().SetTrigger("Idle");
         }
