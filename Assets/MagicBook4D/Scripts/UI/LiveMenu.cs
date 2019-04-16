14918ada76ee96468d31be3571ca599e47ab91ac
Nguyen Danh Dat <datnguyen.firecoals@gmail.com>
2019-04-16 14:10:19 +0700

    New feature: Smooth Scene Transition in Firecoals.SceneTransition

--- Assets/MagicBook4D/Scripts/UI/LiveMenu.cs
+++ Assets/MagicBook4D/Scripts/UI/LiveMenu.cs
@@ -3,7 +3,9 @@
 namespace Firecoals.UI
 {
     using UnityEngine;
-
+    /// <summary>
+    /// Handle texture and audio playing in the Menu Scene
+    /// </summary>
     public class LiveMenu : MonoBehaviour
     {
         private bool _animalIsPlaying = false, _spaceIsPlaying, _colorIsPlaying;
@@ -20,8 +22,10 @@ namespace Firecoals.UI
             if (transform.localPosition.x < 100 && transform.localPosition.x > -100 && _animalIsPlaying == false)
             {
                 //TODO Animal
-                Debug.Log("<color=green>Animal is playing</color>");
+                //Debug.Log("<color=green>Animal is playing</color>");
+                FirecoalsSoundManager.StopAllSounds();
                 FirecoalsSoundManager.PlaySound(animalAudioClip, true);
+                
                 backDrop.mainTexture = Resources.Load<Texture2D>("Bg_texture/menu_bg_animal");
                 _animalIsPlaying = true;
                 _colorIsPlaying = false;
@@ -30,8 +34,8 @@ namespace Firecoals.UI
             if (transform.localPosition.x < -600 && transform.localPosition.x <= -100 && _colorIsPlaying == false)
             {
                 //TODO color
-                Debug.Log("<color=yellow>Color is playing</color>");
-
+                //Debug.Log("<color=yellow>Color is playing</color>");
+                FirecoalsSoundManager.StopAllSounds();
                 FirecoalsSoundManager.PlaySound(colorAudioClip, true);
                 backDrop.mainTexture = Resources.Load<Texture2D>("Bg_texture/menu_bg_color");
                 _animalIsPlaying = false;
@@ -41,7 +45,8 @@ namespace Firecoals.UI
             if (transform.localPosition.x > 600 && transform.localPosition.x >= 100 && _spaceIsPlaying == false)
             {
                 //TODO Space
-                Debug.Log("<color=pink>Space is playing</color>");
+                //Debug.Log("<color=pink>Space is playing</color>");
+                FirecoalsSoundManager.StopAllSounds();
                 FirecoalsSoundManager.PlaySound(spaceAudioClip,true);
                 backDrop.mainTexture = Resources.Load<Texture2D>("Bg_texture/menu_bg_space");
                 _animalIsPlaying = false;
---
+++ Assets/MagicBook4D/Scripts/UI/LoadingSpinner.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+namespace Firecoals.SceneTransition
+{
+    public class LoadingSpinner : MonoBehaviour
+    {
+        public float speed = 5f;
+        private float gondelarm_sin;
+        private void Update()
+        {
+
+            //Quaternion newRotation = Quaternion.AngleAxis(180f, Vector3.forward);
+            //transform.rotation = Quaternion.Slerp(transform.rotation, newRotation, speed);
+            //if (Quaternion.Angle(transform.rotation, newRotation) < 1)
+            //{
+            //    transform.rotation = Quaternion.AngleAxis(0, Vector3.back);
+            //}
+
+            float arm_rot = Mathf.Sin(gondelarm_sin) * 80.0f * 0.5f;
+            gondelarm_sin += Time.deltaTime * speed;
+            transform.localEulerAngles = new Vector3(0.0f, 0.0f, arm_rot);
+        }
+    }
+}
